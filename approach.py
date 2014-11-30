1ffc4b45055224945b8938f9a89823b235394b19
Jorge Valhondo Rama <jrg.valhondo@gmail.com>
2014-11-30 16:29:21 +0100

    Submission 2: dimensionality reduction before SVM with RBF kernel. Classification accuracy: 0.91244. Not an improvement.

--- approach.py
+++ approach.py
@@ -1,19 +1,25 @@
 import numpy as np
 import scipy as sp
 from sklearn import svm
+from sklearn.decomposition import PCA
 
 # Read data sets
 trainData = np.genfromtxt('data/train.csv', delimiter = ',')
 trainLables = np.genfromtxt('data/trainLabels.csv', delimiter = ',')
 testData = np.genfromtxt('data/test.csv', delimiter = ',')
 
+# PCA: dimensionality reduction
+pca = PCA(n_components = 30)
+pcaTrainData = pca.fit_transform(trainData)
+pcaTestData = pca.transform(testData)
+
 # SVC RBF kernel
 clf = svm.SVC()
-clf.fit(trainData, trainLables)
-prediction = clf.predict(testData)
+clf.fit(pcaTrainData, trainLables)
+prediction = clf.predict(pcaTestData)
 
 indexes = range(1, len(prediction) + 1, 1)
 result = np.column_stack((indexes, prediction))
 
 # write to file
-np.savetxt('predictions/pred_1.csv', result.astype(int), fmt = '%s', delimiter = ',', header = 'Id,Solution')
+np.savetxt('predictions/pred_2_pca_svmRBF.csv', result.astype(int), fmt = '%s', delimiter = ',', header = 'Id,Solution')
